adb759715550c14c59da81b2440aee2c7d03c5ba
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2022-06-06 16:00:32 +0430

    Set comment for views

--- Hospital__CBV/Core/hospital/api/v1/views.py
+++ Hospital__CBV/Core/hospital/api/v1/views.py
@@ -10,12 +10,18 @@ from django.shortcuts import get_list_or_404
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def person_views(request):
+    """
+    Show persons in list phones
+    """
     person = Person.objects.all()
     if request.method == 'GET':
         serializer = PersonSerializer(data=person, many=True)
         serializer.is_valid()
         return Response(serializer.data)
     elif request.method == 'POST':
+        """
+        Crate person and phones
+        """
         serializer = PersonSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -25,12 +31,18 @@ def person_views(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
 def person_detail_views(request, national_code):
-    person = get_list_or_404(Person, national_code=national_code)
     if request.method == 'GET':
+        """
+          Show person in list phones
+        """
+        person = get_list_or_404(Person, national_code=national_code)
         serializer = PersonSerializer(data=person, many=True)
         serializer.is_valid()
         return Response(serializer.data)
     elif request.method == 'PUT':
+        """
+        Update person and phones
+        """
         person = Person.objects.get(national_code=national_code)
         serializer = PersonSerializer(person, data=request.data)
         serializer.is_valid(raise_exception=True)
